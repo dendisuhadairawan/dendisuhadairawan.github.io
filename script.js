c1c0713c43d195827343f90db12fe1f9a1669e1d
dendisuhadairawan <dendi.suhada@gmail.com>
2023-11-20 16:48:12 +0700

    merubah

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
+// Tambahkan skrip untuk mengelola splash screen di dalam script.js
 document.addEventListener('DOMContentLoaded', (event) => {
+    // Tampilkan splash screen saat konten dimuat
     showSplashScreen();
+
+    // Setelah beberapa detik, sembunyikan splash screen
     setTimeout(() => {
         hideSplashScreen();
-    }, 2000); 
+    }, 2000); // Sesuaikan waktu tampil splash screen (dalam milidetik)
 });
+
+// Fungsi untuk menampilkan splash screen
 function showSplashScreen() {
     const splashScreen = document.getElementById('splash-screen');
     splashScreen.style.display = 'flex';
 }
+
+// Fungsi untuk menyembunyikan splash screen
 function hideSplashScreen() {
     const splashScreen = document.getElementById('splash-screen');
     splashScreen.style.display = 'none';
 }
 
+
+// Simpan pesanan dalam array
 let orders = [];
+
+// Fungsi untuk menambah pesanan
 function addOrder() {
+    // Dapatkan nilai input
     const selectedType = document.getElementById('type').value;
     const selectedHari = document.getElementById('hari').value;
+
+    // Validasi input
     if (!selectedHari || selectedHari <= 0) {
         alert('Mohon masukkan jumlah hari yang valid.');
         return;
     }
+
+    // Tentukan harga dan diskon berdasarkan jenis mobil dan jumlah hari
     let hargaPerHari;
     let diskon = 0;
 
@@ -46,24 +63,40 @@ function addOrder() {
         default:
             hargaPerHari = 0;
     }
+
+    // Validasi harga
     if (hargaPerHari === 0) {
         alert('Jenis mobil tidak valid.');
         return;
     }
+
+    // Hitung total harga dengan mempertimbangkan diskon
     const totalHarga = selectedHari * hargaPerHari - diskon * hargaPerHari;
+
+    // Buat objek pesanan
     const order = {
         type: selectedType,
         hari: selectedHari,
         totalHarga: totalHarga,
     };
+
+    // Tambahkan pesanan ke array
     orders.push(order);
+
+    // Tampilkan pesanan
     showOrders();
+
+    // Reset formulir
     document.getElementById('type').value = 'Avanza';
     document.getElementById('hari').value = '';
 }
+
+// Fungsi untuk menampilkan pesanan dalam tabel
 function showOrders() {
     const ordersTable = document.getElementById('orders-table');
-    ordersTable.innerHTML = '';
+    ordersTable.innerHTML = ''; // Bersihkan tabel sebelum menambahkan pesanan baru
+
+    // Tampilkan header tabel
     ordersTable.innerHTML += `
         <tr>
             <th>No</th>
@@ -73,6 +106,8 @@ function showOrders() {
             <th>Aksi</th>
         </tr>
     `;
+
+    // Tampilkan setiap pesanan dalam baris tabel
     orders.forEach((order, index) => {
         const orderRow = `
             <tr>
@@ -88,7 +123,11 @@ function showOrders() {
         `;
         ordersTable.innerHTML += orderRow;
     });
+
+    // Hitung total pembayaran
     const totalPembayaran = orders.reduce((total, order) => total + order.totalHarga, 0);
+
+    // Tampilkan total pembayaran di bagian bawah tabel
     const totalRow = `
         <tr>
             <td colspan="3" align="right"><strong>Total Pembayaran:</strong></td>
@@ -97,27 +136,51 @@ function showOrders() {
         </tr>
     `;
     ordersTable.innerHTML += totalRow;
+
+    // Update total pembayaran pada elemen HTML
     document.getElementById('total-amount').innerText = totalPembayaran;
 }
+
+// Fungsi untuk mengedit pesanan
 function editOrder(index) {
+    // Dapatkan pesanan berdasarkan indeks
     const selectedOrder = orders[index];
+
+    // Isi formulir dengan nilai pesanan yang akan diubah
     document.getElementById('type').value = selectedOrder.type;
     document.getElementById('hari').value = selectedOrder.hari;
+
+    // Hapus pesanan dari daftar
     orders.splice(index, 1);
+
+    // Tampilkan ulang pesanan setelah diubah
     showOrders();
 }
+
+// Fungsi untuk menghapus pesanan
 function removeOrder(index) {
+    // Hapus pesanan dari array berdasarkan indeks
     orders.splice(index, 1);
+    
+    // Tampilkan ulang pesanan setelah dihapus
     showOrders();
 }
+
+// Fungsi untuk menghitung pembayaran
 function calculatePayment() {
     const paymentMethod = document.getElementById('payment-method').value;
     const totalPembayaran = orders.reduce((total, order) => total + order.totalHarga, 0);
+
+    // Dapatkan nilai jumlah pembayaran dari formulir
     const paymentAmount = parseFloat(document.getElementById('payment-amount').value);
+
+    // Validasi jumlah pembayaran
     if (isNaN(paymentAmount) || paymentAmount <= 0) {
         alert('Mohon masukkan jumlah pembayaran yang valid.');
         return;
     }
+
+    // Menampilkan detail pembayaran
     const paymentResult = document.getElementById('payment-result');
     paymentResult.innerHTML = `<strong>Detail Pembayaran (${paymentMethod}):</strong><br>`;
     paymentResult.innerHTML += `Total Pembayaran: ${totalPembayaran}<br>`;
@@ -127,6 +190,7 @@ function calculatePayment() {
         paymentResult.innerHTML += 'Cicilan 3 bulan tanpa bunga.';
     }
 }
+// Registrasi Service Worker
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('service-worker.js', { scope: '/' })
         .then((registration) => {
@@ -137,10 +201,16 @@ if ('serviceWorker' in navigator) {
         });
 }
 function printReceipt() {
+    // Sembunyikan elemen-elemen yang tidak ingin dicetak
     document.querySelector('body').classList.add('printing');
     document.getElementById('splash-screen').style.display = 'none';
+    document.getElementById('booking-form').style.display = 'none';
+
+    // Panggil fungsi pencetakan browser
     window.print();
+
+    // Tampilkan kembali elemen-elemen yang disembunyikan setelah pencetakan selesai
     document.querySelector('body').classList.remove('printing');
     document.getElementById('splash-screen').style.display = 'flex';
-    
+    document.getElementById('booking-form').style.display = 'block';
 }
